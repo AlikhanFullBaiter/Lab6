26ca843a9d29355f6c29d7a6e87c9a0956fd423f
Alikhan <astanaproteam123@gmail.com>
2023-06-01 17:23:56 +0600

    fixing

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -3,6 +3,10 @@ package src;
 public class DijkstraSearch<V> implements Search<V> {
     private WeightedGraph<V> graph;
 
+    public DijkstraSearch(WeightedGraph<V> graph) {
+
+    }
+
     public void BreadthFirstSearch(WeightedGraph<V> graph) {
         this.graph = graph;
     }
--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,44 +1,39 @@
 package src;
-
 import java.security.spec.RSAOtherPrimeInfo;
 import java.util.*;
-
 public class WeightedGraph<V> {
     private Map<Vertex<V>, List<Vertex<V>>> adjacencyList;
-
     public WeightedGraph() {
         adjacencyList = new HashMap<>();
     }
-
     public void addVertex(Vertex<V> vertex) {
         adjacencyList.put(vertex, new LinkedList<>());
     }
-
     public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
         validateVertex(source);
         validateVertex(destination);
 
         source.addAdjacentVertex(destination, weight);
         destination.addAdjacentVertex(source, weight);
+
+        adjacencyList.get(source).add(destination);
+        adjacencyList.get(destination).add(source);
     }
+
 
     private void validateVertex(Vertex<V> vertex) {
         if (!adjacencyList.containsKey(vertex))
             throw new IllegalArgumentException("Vertex " + vertex + " is not in the graph");
     }
-
     public void removeEdge(Vertex<V> source, Vertex<V> destination) {
         validateVertex(source);
         validateVertex(destination);
-
         source.getAdjacentVertices().remove(destination);
         destination.getAdjacentVertices().remove(source);
     }
-
     public boolean hasEdge(Vertex<V> source, Vertex<V> destination) {
         validateVertex(source);
         validateVertex(destination);
-
         return source.getAdjacentVertices().containsKey(destination);
     }
     public List<Vertex<V>> getNeighbors(Vertex<V> vertex) {
@@ -56,16 +51,21 @@ public class WeightedGraph<V> {
         Queue<Vertex<V>> queue = new LinkedList<>();
         queue.add(start);
         visited.put(start, true);
+        queue.add(start);
 
         while (!queue.isEmpty()) {
             Vertex<V> currentVertex = queue.poll();
             System.out.println(currentVertex.getData() + " ");
+            Vertex<V> vertex = queue.poll();
+            System.out.print(vertex.getData() + " ");
 
             List<Vertex<V>> neighbors = adjacencyList.get(currentVertex);
+            List<Vertex<V>> neighbors = adjacencyList.get(vertex);
             for (Vertex<V> neighbor : neighbors) {
                 if (!visited.get(neighbor)) {
                     queue.add(neighbor);
                     visited.put(neighbor, true);
+                    queue.add(neighbor);
                 }
             }
         }
@@ -92,30 +92,42 @@ public class WeightedGraph<V> {
             if (distances.get(currentVertex) == Double.POSITIVE_INFINITY) {
                 break;
             }
+            PriorityQueue<Vertex<V>> priorityQueue = new PriorityQueue<>(Comparator.comparingDouble(distances::get));
+            priorityQueue.add(start);
 
-            List<Vertex<V>> neighbors = adjacencyList.get(currentVertex);
-            for (Vertex<V> neighbor : neighbors) {
-                double newDistance = distances.get(currentVertex) + currentVertex.getAdjacentVertices().get(neighbor);
-                if (newDistance < distances.get(neighbor)) {
-                    queue.remove(neighbor);
-                    distances.put(neighbor, newDistance);
-                    previous.put(neighbor, currentVertex);
-                    queue.add(neighbor);
+            while (!priorityQueue.isEmpty()) {
+                Vertex<V> vertex = priorityQueue.poll();
+                double distance = distances.get(vertex);
+
+                for (Map.Entry<Vertex<V>, Double> entry : vertex.getAdjacentVertices().entrySet()) {
+                    Vertex<V> neighbor = entry.getKey();
+                    double weight = entry.getValue();
+                    double newDistance = distance + weight;
+
+                    List<Vertex<V>> neighbors = adjacencyList.get(currentVertex);
+                    for (Vertex<V> neighbor : neighbors) {
+                        double newDistance = distances.get(currentVertex) + currentVertex.getAdjacentVertices().get(neighbor);
+                        if (newDistance < distances.get(neighbor)) {
+                            queue.remove(neighbor);
+                            priorityQueue.remove(neighbor);
+                            distances.put(neighbor, newDistance);
+                            previous.put(neighbor, currentVertex);
+                            queue.add(neighbor);
+                            priorityQueue.add(neighbor);
+                        }
+                    }
+                }
+                return distances;
+            }
+            public void printGraph() {
+                for (Map.Entry<Vertex<V>, List<Vertex<V>>> entry : adjacencyList.entrySet()) {
+                    Vertex<V> vertex = entry.getKey();
+                    List<Vertex<V>> neighbors = entry.getValue();
+                    System.out.print("Vertex " + vertex.getData() + " is connected to: ");
+                    for (Vertex<V> neighbor : neighbors) {
+                        System.out.print(neighbor.getData() + " ");
+                    }
+                    System.out.println();
                 }
             }
         }
-        return distances;
-    }
-
-    public void printGraph() {
-        for (Map.Entry<Vertex<V>, List<Vertex<V>>> entry : adjacencyList.entrySet()) {
-            Vertex<V> vertex = entry.getKey();
-            List<Vertex<V>> neighbors = entry.getValue();
-            System.out.print("Vertex " + vertex.getData() + " is connected to: ");
-            for (Vertex<V> neighbor : neighbors) {
-                System.out.print(neighbor.getData() + " ");
-            }
-            System.out.println();
-        }
-    }
-}
