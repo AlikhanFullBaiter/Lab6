88ea11bb69925c268742f59eb45bc2e5ca92a6af
Alikhan <astanaproteam123@gmail.com>
2023-06-01 17:03:38 +0600

    constructor

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,4 +1,13 @@
 package src;
 
-public class WeightedGraph {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeightedGraph<V> {
+    private Map<Vertex<V>, List<Vertex<V>>> adjacencyList;
+
+    public WeightedGraph() {
+        adjacencyList = new HashMap<>();
+    }
 }
