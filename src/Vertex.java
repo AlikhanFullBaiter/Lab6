7e5eedb2166322594edfb97ed6b75f510ba7f9f6
Alikhan <astanaproteam123@gmail.com>
2023-06-01 16:59:50 +0600

    constructor

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,8 +1,14 @@
 package src;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjacentVertices;
+
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
 }
